52bd5b1b6c4eadd36cdfb4c9dac9ed6a933cf44a
001faaiz <adewaleabdulafeez@gmail.com>
2022-12-13 19:27:30 -0800

    starting out at zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print _printchar
+ * I have done this watching series of tutorial
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
