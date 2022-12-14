ade49a175393e20f39368cd6d68b3719d01a3227
001faaiz <adewaleabdulafeez@gmail.com>
2022-12-14 14:32:15 -0800

    task 7 correct

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,8 @@ int _islower (int c);
 int _isalpha (int c);
 int print_sign (int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int last);
+void jack_bauer(void x);
 
 
 #endif
